b92555b8c7e84c4862e38e5741b0be4be0baab64
Eros Casas <ecasas182015@gmail.com>
2023-01-30 16:02:29 -0600

    started web app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import logo from './logo.svg';
+import NavBar from './components/navbar/navbar.js';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <NavBar />
+      <button type="button" class="btn btn-primary">Primary</button>
+      <button type="button" class="btn btn-secondary">Secondary</button>
+      <button type="button" class="btn btn-success">Success</button>
+      <button type="button" class="btn btn-danger">Danger</button>
+      <button type="button" class="btn btn-warning">Warning</button>
+      <button type="button" class="btn btn-info">Info</button>
+      <button type="button" class="btn btn-light">Light</button>
+      <button type="button" class="btn btn-dark">Dark</button>
+
+      <button type="button" class="btn btn-link">Link</button>
+
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,7 +32,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,11 @@
+import './navbar.css';
+
+function NavBar() {
+    return (
+        <div>
+            <h1>"This is where navbar will be.</h1>
+        </div>
+    );
+}
+
+export default NavBar;
