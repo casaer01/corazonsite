18b90c6a24856b58a364b7602175c2d66ff01b91
Eros Casas <ecasas182015@gmail.com>
2023-02-01 19:19:18 -0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -7,32 +7,6 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      <button type="button" class="btn btn-primary">Primary</button>
-      <button type="button" class="btn btn-secondary">Secondary</button>
-      <button type="button" class="btn btn-success">Success</button>
-      <button type="button" class="btn btn-danger">Danger</button>
-      <button type="button" class="btn btn-warning">Warning</button>
-      <button type="button" class="btn btn-info">Info</button>
-      <button type="button" class="btn btn-light">Light</button>
-      <button type="button" class="btn btn-dark">Dark</button>
-
-      <button type="button" class="btn btn-link">Link</button>
-
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
     </div>
   );
 }
